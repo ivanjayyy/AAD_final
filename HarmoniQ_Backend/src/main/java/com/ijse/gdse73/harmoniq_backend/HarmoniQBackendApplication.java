488080827bdeaaa4fc7684fb0b3e961db2a0e5b4
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-03-01 18:15:19 +0530

    make frontend/backend folders

---
+++ HarmoniQ_Backend/src/main/java/com/ijse/gdse73/harmoniq_backend/HarmoniQBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ijse.gdse73.harmoniq_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmoniQBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmoniQBackendApplication.class, args);
+    }
+
+}
---
+++ HarmoniQ_Backend/src/test/java/com/ijse/gdse73/harmoniq_backend/HarmoniQBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.gdse73.harmoniq_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmoniQBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
